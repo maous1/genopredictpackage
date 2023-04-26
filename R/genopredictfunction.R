5b24e4c529ef58f6cdf56dd3e8c15fc351b74864
MBonjean <67752577+maous1@users.noreply.github.com>
2023-04-26 17:32:38 +0200

    Update genopredictfunction.R

--- R/genopredictfunction.R
+++ R/genopredictfunction.R
@@ -2,15 +2,15 @@
 #'
 #' @param df
 #' @param classe
-#' @param min_prevalence_percentage
 #' @param k
+#' @param min_prevalence
 #'
 #' @return
 #' @export
 #' @import Biostrings
 #' @import purrr
 #' @examples
-genopredictfunction <- function(df , k , classe, min_prevalence_percentage = 10){
+genopredictfunction <- function(df , k , classe, min_prevalence = 10){
   if(min_prevalence_percentage > 100){message("more than 100%")}
   if(min_prevalence_percentage < 0){message("less than 100%")}
   print("classification")
@@ -18,7 +18,7 @@ genopredictfunction <- function(df , k , classe, min_prevalence_percentage = 10)
   print("kmer")
   data <- map(.x = as.list(data),.f = function(.x) kmer(.x,k))
   print("df_kmer")
-  print(paste0("the number of minimal sequence = ",min_prevalence_percentage*dim(df)[1]/100))
-  data <- df_kmer(list = data,minimum = min_prevalence_percentage*dim(df)[1]/100)
+  print(paste0("the number of minimal sequence = ",min_prevalence*dim(df)[1]/100))
+  data <- df_kmer(list = data,minimum = min_prevalence*dim(df)[1]/100)
   return(data)
 }
